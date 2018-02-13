525eb7f0c587fd54a0af6cb286d71c381dddb407
Rajan <rajan2275@gmail.com>
2018-02-13 15:37:16 +0000

    euclidean score calculation.

--- Data Scraping/scrape_website.py
+++ Data Scraping/scrape_website.py
@@ -8,6 +8,7 @@ from selenium import webdriver
 urls=["https://rajan2275.github.io/Python-Design-Patterns",
       'https://github.com/rajan2275/Python-Design-Patterns',
       'https://github.com/rajan2275/Data-Science-Projects',
+      'https://github.com/rajan2275/Python-Test-Driven-Development',
       'https://www.google.co.uk/search?biw=1517&bih=707&ei=pUdjWpr0FYjZwQKH3qP4Ag&q=python+design+patterns+github+rajan2275&oq=python+design+patterns+github+rajan2275&gs_l=psy-ab.3...5874.10000.0.10333.11.11.0.0.0.0.84.813.11.11.0....0...1c.1.64.psy-ab..0.6.457...33i21k1j33i160k1.0.swmQ8Vhyv7Q']
 
 for n in range(0, 50):
---
+++ Recommendation Engine/euclidean_score.py
@@ -0,0 +1,24 @@
+
+import json
+import numpy as np
+# Returns the Euclidean distance score between user1 and user2
+def euclidean_score(dataset, user1, user2):
+    # Check user in dataset.
+    if user1 not in dataset: raise Exception('User ' + user1 + ' not in dataset.')
+    if user2 not in dataset: raise Exception('User ' + user2 + ' not in dataset')
+    rated_by_both = {item:1 for item in dataset[user1] if item in dataset[user2]}
+
+    if len(rated_by_both) == 0: return 0
+    squared_differences = [np.square(dataset[user1][item] - dataset[user2][item]) for item in dataset[user1] if item in dataset[user2]]
+
+    return 1 / (1 + np.sqrt(np.sum(squared_differences)))
+
+if __name__=='__main__':
+
+    filename = 'movie_ratings.json'
+    with open(filename, 'r') as f: data = json.loads(f.read())
+    user1 = 'John Carson'
+    user2 = 'Michelle Peterson'
+
+    print('\nEuclidean score:')
+    print(euclidean_score(data, user1, user2))
