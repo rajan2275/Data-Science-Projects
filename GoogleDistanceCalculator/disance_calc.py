04d6409d1614fd3fdd3e14704e58d8393e5705d7
Rajan <rajan2275@gmail.com>
2018-03-29 23:21:19 +0100

    Google distance calculator.

--- GoogleDistanceCalculator/disance_calc.py
+++ GoogleDistanceCalculator/disance_calc.py
@@ -3,6 +3,8 @@ import json
 import os
 from sys import platform
 import pandas as pd
+import csv
+from types import SimpleNamespace as Namespace
 
 
 from urllib.parse import urlencode
@@ -21,29 +23,39 @@ filepath = path+'/GoogleDistanceCalculator/data_files/' \
            if platform == 'linux' or platform == 'linux2' \
            else path+'\\GoogleDistanceCalculator\\data_files\\'
 
-filename = 'origins_and_destinations.csv'
+filename = 'input_origins_and_destinations.csv'
 
 final_path = filepath + filename
 input_df = pd.read_csv(final_path)
 
-output_df = pd.DataFrame(columns=['S. No', 'origin', 'destination', 'distance', 'raw_response'])
+output_df = pd.DataFrame(columns=['S.No.', 'origin', 'destination', 'distance', 'raw_response'])
 
 url_endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json'
+fields = ['S. No', 'origin', 'destination', 'distance', 'raw_response' ]
+error_message = ''
+distance = ''
 
+with open(filepath + 'output_distances.csv','w', newline='') as newFile:
+    newFileWriter = csv.writer(newFile)
+    newFileWriter.writerow(['S.No.', 'origin', 'destination', 'distance', 'raw_response', 'Error' ])
 
-for index, row in input_df.iterrows():
-    mydict = {'units'       : measurement_unit,
-              'origins'     : row['origin_address'],
-              'destinations': row['destination_address'],
-              'key'         : google_key}
+    for index, row in input_df.iterrows():
+        mydict = {'units'       : measurement_unit,
+                  'origins'     : row['origin_address'],
+                  'destinations': row['destination_address'],
+                  'key'         : google_key}
+        error_message = 'No Error'
+        try:
+            resp = requests.get(url_endpoint, params=mydict)
+            if(resp.ok):
+                jData = json.loads(resp.text)
+                if not jData['rows'][0]['elements'][0]['Status'] == 'NOT_FOUND':
+                    distance = jData['rows'][0]['elements'][0]['distance']['text']
+        except Exception as e:
+                error_message = 'Error getting distance.'
+                pass
 
-    resp = requests.get(url_endpoint, params=mydict)
-
-
-    if(resp.ok):
-        jData = json.loads(resp.text)
-
-
-
-
-    print(jData)
+        combined_row = [row['S.No.'], row['origin_address'], row['destination_address'], distance , str(jData), error_message ]
+        newFileWriter.writerow(combined_row)
+        #jData['rows'][0]['elements'][0]
+        print(jData)
