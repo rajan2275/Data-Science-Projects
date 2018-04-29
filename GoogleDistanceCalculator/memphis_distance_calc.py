9db7b2078cbd57338721f8df7b4ca2794b71838b
Rajan Sharma <rajan2275@gmail.com>
2018-04-29 18:46:08 +0100

    distance calculator memphis

--- GoogleDistanceCalculator/memphis_distance_calc.py
+++ GoogleDistanceCalculator/memphis_distance_calc.py
@@ -7,8 +7,6 @@ import csv
 import xlrd
 import xlsxwriter
 
-
-
 path = os.path.dirname(os.getcwd())
 
 # Key and unit needs to be changed.
@@ -19,8 +17,6 @@ filepath = path+'/GoogleDistanceCalculator/data_files/' \
            if platform == 'linux' or platform == 'linux2' \
            else path+'\\GoogleDistanceCalculator\\data_files\\'
 
-filename = 'input_origins_and_destinations.csv'
-
 filename_origins  = 'input_origins.xlsx'
 filename_destinations ='input_destinations.xlsx'
 filename_output = 'output_matrix.xlsx'
@@ -29,23 +25,16 @@ final_path_origins = filepath + filename_origins
 final_path_destinations = filepath + filename_destinations
 final_path_output = filepath + filename_output
 
-print(final_path_origins)
-
 input_df_origins = pd.read_excel(final_path_origins, sheet_name='input_origins')
 input_df_destinations = pd.read_excel(final_path_destinations, sheet_name='input_destinations')
 
-#output_df = pd.DataFrame(columns=['S.No.', 'origin', 'destination', 'distance', 'raw_response'])
-
 url_endpoint = 'https://maps.googleapis.com/maps/api/distancematrix/json'
-#fields = ['S. No', 'origin', 'destination', 'distance', 'raw_response' ]
-fields = ['S. No', 'origin']
 error_message = ''
 distance = ''
 
 workbook =xlsxwriter.Workbook(final_path_output)
 worksheet = workbook.add_worksheet()
-# values = ['3', '6', '9']
-# worksheet.write(1, 1, '\n'.join(values))
+
 
 excel_row = 1
 excel_column = 1
@@ -55,7 +44,7 @@ count = 20
 for index, row in input_df_destinations.iterrows():
     destination = row['destinations']
     destination_id = row['restaurant.id']
-    worksheet.write(excel_row, 0, destination)
+    worksheet.write(excel_row, 0, destination_id)
     for i, r in input_df_origins.iterrows():
         origin = r['origins']
         origin_id = r['restaurant.id']
@@ -68,22 +57,23 @@ for index, row in input_df_destinations.iterrows():
             resp = requests.get(url_endpoint, params=mydict)
             if(resp.ok):
                 jData = json.loads(resp.text)
+                print(jData)
                 distance = jData['rows'][0]['elements'][0]['distance']['text']
+                distance = float(distance.replace('mi','').strip())
         except Exception as e:
                 error_message = 'Error'
                 pass
 
         if index == 0:
-            worksheet.write(0, excel_column, origin)
+            worksheet.write(0, excel_column,  origin_id)
 
         worksheet.write(excel_row, excel_column, distance)
 
         excel_column = excel_column + 1
     excel_row = excel_row + 1
     excel_column = 1
-    #if index == 5: break
+    if index == 5: break
 workbook.close()
-
 
 
 
