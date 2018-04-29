ce8607895c9dfae27a23384e381be62397cff696
Rajan Sharma <rajan2275@gmail.com>
2018-04-29 13:06:58 +0100

    distance calculator

--- GoogleDistanceCalculator/memphis_distance_calc.py
+++ GoogleDistanceCalculator/memphis_distance_calc.py
@@ -13,7 +13,7 @@ path = os.path.dirname(os.getcwd())
 
 # Key and unit needs to be changed.
 # ----------------------------------
-google_key = 'AIzaSyBOpfbJt97X-nUq76lbVyAnukrwKgFQDa4'
+google_key = ''
 measurement_unit = 'imperial'
 filepath = path+'/GoogleDistanceCalculator/data_files/' \
            if platform == 'linux' or platform == 'linux2' \
@@ -47,65 +47,47 @@ worksheet = workbook.add_worksheet()
 # values = ['3', '6', '9']
 # worksheet.write(1, 1, '\n'.join(values))
 
-#worksheet.write(row, col,     'First Name')
-
 excel_row = 1
 excel_column = 1
 
-for index, row in input_df_destinations:
+count = 20
+
+for index, row in input_df_destinations.iterrows():
     destination = row['destinations']
+    destination_id = row['restaurant.id']
     worksheet.write(excel_row, 0, destination)
-    for i, r in input_df_origins:
+    for i, r in input_df_origins.iterrows():
         origin = r['origins']
+        origin_id = r['restaurant.id']
         mydict = {'units': measurement_unit,
                   'origins': origin,
                   'destinations': destination,  # row['destination_address'],
                   'key': google_key}
         error_message = 'No Error'
-        # try:
-        #     resp = requests.get(url_endpoint, params=mydict)
-        #     if(resp.ok):
-        #         jData = json.loads(resp.text)
-        #         distance = jData['rows'][0]['elements'][0]['distance']['text']
-        # except Exception as e:
-        #         error_message = 'Error'
-        #         pass
-        worksheet.write(excel_row, excel_column, origin)
+        try:
+            resp = requests.get(url_endpoint, params=mydict)
+            if(resp.ok):
+                jData = json.loads(resp.text)
+                distance = jData['rows'][0]['elements'][0]['distance']['text']
+        except Exception as e:
+                error_message = 'Error'
+                pass
 
+        if index == 0:
+            worksheet.write(0, excel_column, origin)
 
+        worksheet.write(excel_row, excel_column, distance)
 
-
-
-
-
-
-# with open(final_path_output,'w', newline='') as newFile:
-#    # newFileWriter =    csv.writer(newFile)
-#   #  newFileWriter.writerow(input_df['origins'])
-#
-#     for index, row in input_df.iterrows():
-#         mydict = {'units'       : measurement_unit,
-#                   'origins'     : row['origins'],
-#                   'destinations': '2901 NE Blakeley St Seattle, WA 98105',  #row['destination_address'],
-#                   'key'         : google_key}
-#         error_message = 'No Error'
-#         try:
-#             resp = requests.get(url_endpoint, params=mydict)
-#             if(resp.ok):
-#                 jData = json.loads(resp.text)
-#                 distance = jData['rows'][0]['elements'][0]['distance']['text']
-#         except Exception as e:
-#                 error_message = 'Error getting distance.'
-#                 pass
-#         #combined_row = [row['origins'], '2901 NE Blakeley St Seattle, WA 98105', distance , str(jData), error_message ]
-#         #newFileWriter.writerow(combined_row)
-#
-#         combined_row = [row['origins'], '2901 NE Blakeley St Seattle, WA 98105', distance, str(jData), error_message]
-#
-#         if index == 0:
-#             worksheet.write(row, row['origins'])
-#
-#         worksheet.write(row, 'test', 'First Name')
-#
-#         print(jData)
+        excel_column = excel_column + 1
+    excel_row = excel_row + 1
+    excel_column = 1
+    #if index == 5: break
 workbook.close()
+
+
+
+
+
+
+
+
